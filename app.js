76a936c4685863a07ee9aad0e298617a703e93f0
suryansh1801 <87513522+suryansh1801@users.noreply.github.com>
2023-06-18 00:47:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get("/", async function (req, res) {
 
     try {
       const weatherKey = process.env.WEATHER_KEY
-      const locKey = process.env.LOCATION_KEY
+      const locKey = c8b7f09f7d91e13e0793a7f3c6ce87e4
 
       const ip = req.ip
 
